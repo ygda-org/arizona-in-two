5851c2e3d0b8cb640d38b443a190ebf9347f68eb
MrPuffyWuff <jasper13.com@gmail.com>
2026-05-02 02:20:26 -0400

    KILLED JACKIE-ISLEEP
    self explanatory ofc

--- globals/gamestate.gd
+++ globals/gamestate.gd
@@ -6,11 +6,19 @@ var player_health: int = 1000
 
 var player_can_take_damage : bool = true
 
+signal player_dead
+
 func reset():
 	player_health = 1000
 
 func damage_player(amount : int):
 	player_can_take_damage = false
+	
+	if player_health <= amount:
+		player_health = 0
+		player_dead.emit()
+		return
+	
 	player_health -= amount
 	player.set_iframe_shader(true)
 	player.iFrameTimer.start()
--- ui/gui.gd
+++ ui/gui.gd
@@ -7,6 +7,7 @@ class_name GUI
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Fade.color.a = 1.0
+	Gamestate.player_dead.connect(game_over)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,3 +18,10 @@ func fade_in():
 
 func fade_out():
 	$AnimationPlayer.play("fade_out")
+
+func game_over():
+	fade_out()
+	await animation_player.animation_finished
+	$Fade/LoseLabel.visible = true
+	get_tree().paused = true
+	
