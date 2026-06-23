3f8e6ab81bc5811774ecda419832e7c8b324106a
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-06-23 13:09:32 -0400

    more camera functionality

--- player/camera.gd
+++ player/camera.gd
@@ -6,6 +6,12 @@ const FOLLOW_STRENGTH = 100
 var shapes = []
 var monitors = []
 
+var target_glide_position
+var glide_speed
+var lock_on_end
+
+var camera_lock: bool = false
+
 func _ready():
 	$Camera2D.zoom = Vector2(zoom,zoom)
 	for node in get_children():
@@ -20,6 +26,15 @@ func _ready():
 			monitors.append(monitor)
 
 func _physics_process(delta):
+	if camera_lock:
+		return
+	if glide_speed:
+		velocity = global_position.direction_to(target_glide_position) * glide_speed
+		position += velocity * delta
+		if (position-target_glide_position).length() < 10:
+			glide_speed = 0
+			set_camera_lock(lock_on_end)
+		return
 	#velocity = global_position.lerp(get_parent().global_position, delta*FOLLOW_STRENGTH)
 	velocity = FOLLOW_STRENGTH * global_position.direction_to(get_parent().global_position)
 	if (global_position - get_parent().global_position).length() < 30:
@@ -39,3 +54,10 @@ func _physics_process(delta):
 		if not area.get_overlapping_bodies():
 			shapes[i].disabled = false
 			
+func set_glide_position(global_pos: Vector2, speed: float, lock_on_finish:bool):
+	target_glide_position = global_pos
+	glide_speed = speed
+	lock_on_end = lock_on_finish
+
+func set_camera_lock(boolean: bool):
+	camera_lock = boolean
