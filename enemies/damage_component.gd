5a471972a971e4cb547f56945d886c6ef280cb57
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-08-06 12:40:55 -0400

    minimum attack delay

--- enemies/damage_component.gd
+++ enemies/damage_component.gd
@@ -8,10 +8,16 @@ class_name DamageComponent
 @export var suicide_on_death: bool = false
 @onready var parent : CollisionObject2D = get_parent()
 
+var min_attack_delay_timer : Timer = null
+
 func _ready() -> void:
 	if suicide_on_death:
 		assert(parent.has_method("suicide"), 'ERROR: No suicide method found')
 	assert(parent.has_method("damaged_sequence"), 'ERROR: No damaged_sequence method found')
+	
+	for node in get_parent().get_children():
+		if node.name == "MinAttackDelayTimer":
+			min_attack_delay_timer = node
 
 func accept_bullet_(bullet: Bullet):
 	var damage_from_bullet = bullet.damage #exported damage
@@ -35,10 +41,21 @@ func _physics_process(_delta: float) -> void:
 		var collision : KinematicCollision2D = parent.get_slide_collision(i)
 		var obj := collision.get_collider()
 		# Player check
-		if obj is Player:
-			if obj.is_in_group("Player") and GameState.player_can_take_damage:
-				var normal := collision.get_normal()
-				obj.velocity -= normal * knockback_strength
-				GameState.damage_player(damage)
-				if suicide_on_death:
-					parent.suicide()
+		if obj is not Player:
+			continue
+		if not obj.is_in_group("Player") or not GameState.player_can_take_damage:
+			continue
+		if min_attack_delay_timer != null:
+			if min_attack_delay_timer.time_left == 0:
+				min_attack_delay_timer.start()
+				deal_damage(collision)
+		else:
+			deal_damage(collision)
+			
+func deal_damage(collision : KinematicCollision2D):
+	var obj := collision.get_collider()
+	var normal := collision.get_normal()
+	obj.velocity -= normal * knockback_strength
+	GameState.damage_player(damage)
+	if suicide_on_death:
+		parent.suicide()
