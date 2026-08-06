5450948382d199961450c5865e2b8c12e43383a6
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-08-06 12:52:26 -0400

    cactus can die now with a animation :)

--- world/decor/destroyable_object.gd
+++ world/decor/destroyable_object.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _ready():
+	animated_sprite_2d.play("default")
 	$DamageComponent.health = 1.0
 
 func damaged_sequence():
 	pass # Hurt Animation
 
 func suicide():
-	queue_free() # Death Animation
+	animated_sprite_2d.play("death")
