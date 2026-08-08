8cb669b86de3e86a2831d076719cab14fd30792c
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-07 21:56:11 -0400

    jqwfmwofwe

--- player/permanent_upgrades/bullet_upgrade.gd
+++ player/permanent_upgrades/bullet_upgrade.gd
@@ -10,13 +10,12 @@ func _ready():
 
 
 func _on_body_entered(_body):
-	print('hi')
 	GameState.player_max_bullet_strength = upgrade_level
 	if upgrade_level == 2:
 		$Sprite2D.visible = false
 		set_deferred("monitoring", false)
 		var gate = load("uid://bc4df4v86bpt2").instantiate()
-		add_child(gate)
+		call_deferred("add_child", gate)
 		gate.position.y += 48
 	else:
 		queue_free()
