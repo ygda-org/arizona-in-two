0fcc98392261038692ae1a90aa5467d593940d23
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-08-11 18:28:48 -0400

    wow main menu animated.

--- ui/menu.gd
+++ ui/menu.gd
@@ -2,12 +2,12 @@ extends Control
 
 func _ready() -> void:
 	$Fade.color.a = 0.0
-
+	
 func _on_start_pressed() -> void:
 	SFX.play(SFX.Id.BUTTON_CLICK)
 	
-	$AnimationPlayer.play("fade_out")
-	await $AnimationPlayer.animation_finished
+	$FadeOutAnimationPlayer.play("fade_out")
+	await $FadeOutAnimationPlayer.animation_finished
 	var _game: PackedScene = load("uid://bvhckihcs8a5l")
 	# Use change_zone to also fix the camera to the map's bounds
 	SceneSwitcher.switch_scene("res://world/desert_area/desert_A.tscn", "FromGrass")
@@ -32,3 +32,12 @@ func _on_settings_mouse_entered():
 	
 func _on_quit_mouse_entered():
 	SFX.play(SFX.Id.BUTTON_HOVER)
+
+func _on_birds_1_timer_timeout():
+	$Birds1Timer.wait_time = randf_range(10,30)
+	$Birds1Timer.start()
+	$Birds1AnimationPlayer.play("birds_1")
+
+func _on_birds_2_timer_timeout():
+	$Birds1Timer.wait_time = randf_range(10,30)
+	$Birds2AnimationPlayer.play("birds_2")
