19ed2e70f076d78b0b09cef558df090e7b81f9e1
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-08-12 08:05:47 -0400

    placeholder super cool main menu thing

--- ui/menu.gd
+++ ui/menu.gd
@@ -2,10 +2,26 @@ extends Control
 
 func _ready() -> void:
 	$Fade.color.a = 0.0
+	$Bullet.visible = false
+	$Explosion.visible = false
+	$"2TextPivot/2TextLeft".visible = false
+	$"2TextPivot/2TextRight".visible = false
 	
 func _on_start_pressed() -> void:
 	SFX.play(SFX.Id.BUTTON_CLICK)
-	
+	$JackieAnimationPlayer.play("return_to_shoot")
+	$ArizonaTextAnimationPlayer.play("return_to_shoot")
+	await get_tree().create_timer(0.25).timeout
+	SFX.play(SFX.Id.GUNSHOT)
+	$ShootAnimationPlayer.play("shoot")
+	$Explosion.visible = true
+	$Explosion.play("default")
+	await get_tree().create_timer(0.125).timeout
+	$Explosion.visible = false
+	$"2TextPivot/2Text".visible = false
+	$"2TextPivot/2TextLeft".visible = true
+	$"2TextPivot/2TextRight".visible = true
+	await get_tree().create_timer(0.125).timeout
 	$FadeOutAnimationPlayer.play("fade_out")
 	await $FadeOutAnimationPlayer.animation_finished
 	var _game: PackedScene = load("uid://bvhckihcs8a5l")
