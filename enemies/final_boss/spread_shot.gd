3c98134157a7f19bd3112c27b3d50f54136747c7
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-19 17:58:32 -0400

    stuffs

--- enemies/final_boss/spread_shot.gd
+++ enemies/final_boss/spread_shot.gd
@@ -4,16 +4,22 @@ extends Node2D
 
 const BULLET = preload("uid://bb7l55p2q3sp5")
 
+const SHOT_DIRECTIONS = [Vector2i(1,0), Vector2i(1,1), 
+Vector2i(0,1), Vector2i(-1,1),
+Vector2i(-1,0), Vector2i(-1,-1),
+Vector2i(0,-1), Vector2i(1,-1)]
+
 func activate():
 	var player = GameState.player
 	var dir = global_position.direction_to(player.global_position)
-	if abs(dir.x) > abs(dir.y):
-		dir = Vector2i(dir.x/abs(dir.x), 0)
-	else:
-		dir = Vector2i(0, dir.y/abs(dir.y))
+	var closest_dir = SHOT_DIRECTIONS[0]
+	for direction in SHOT_DIRECTIONS:
+		if Vector2(direction).normalized().distance_to(dir) < Vector2(closest_dir).normalized().distance_to(dir):
+			closest_dir = direction
+	dir = closest_dir
 	get_parent().get_parent().play_anim_by_dir("attack_", dir)
 	await get_tree().create_timer(0.5).timeout
-	dir = Vector2(dir)
+	dir = Vector2(dir).normalized()
 	for i in range(3):
 		var bullet = BULLET.instantiate()
 		bullet.velocity = bullet_speed * dir.rotated((i-1) * PI/4)
